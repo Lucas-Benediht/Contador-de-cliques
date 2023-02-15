f0082ea6edc7f1a138b5eea44348ca1bf4340420
Lucas Benediht Caldeira <110697669+Lucas-Benediht@users.noreply.github.com>
2023-02-15 18:18:59 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ function contar(){
     contador ++ // Estou dizendo que o contador é igual contador +1
     resultado.innerHTML = `<p>O contador está com: <mark>${contador}</mark> cliques.</p>`
 }
+
 function zerar(){
     contador = 0
     resultado.innerHTML = null
